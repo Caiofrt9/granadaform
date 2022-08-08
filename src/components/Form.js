853246bcaa9651fb9b502bd9cbf04f7782641282
Caio F <silvacaio295@gmail.com>
2022-08-08 08:17:22 -0300

    adicionando detalhes deplano

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,12 +11,17 @@ const Form = () => {
     console.log('alo')
   }
 
+  const showDetails = () => {
+    console.log('teste')
+  }
+
   return (
     <div id="container">
       <div className="title">
         <img className="logo" src={logo} />
-        <h2>Faça parte da nossa atlética.</h2>
+        <h2>Cadastro de Associados - Atlética Granada</h2>
       </div>
+
       <div className="form">
         <form
           action="https://formsubmit.co/silvacaio295@gmail.com"
@@ -53,9 +58,17 @@ const Form = () => {
             </div>
           </div>
 
-          <div className="plano">
+          <div className="plan">
             <div className="plan-title">
-              <h2>Escolha seu plano de associação</h2>
+              <h2>Escolha seu plano de associação.</h2>
+              <h4>
+                Plano 1: Descontos em lojas parceiras, descontos em produtos da
+                atlética, descontos em jogos da atlética.
+              </h4>
+              <h4>
+                Plano 2: Todos os benefícios do Plano 1 + direito a 1 Kit Caneca
+                + Tirante sem custos adicionais.
+              </h4>
             </div>
 
             <div className="plan-group">
@@ -76,47 +89,47 @@ const Form = () => {
           </div>
 
           <div className="sports">
-            <h2>Esportes que você pratica</h2>
+            <h2>Esportes que você pratica.</h2>
             <div className="sport-box">
               <div className="check-box">
-                <input id="checkbox-1" type="checkbox" />
+                <input id="checkbox-1" type="checkbox" name="futebol" />
                 <label for="checkbox-1">Futebol</label>
               </div>
               <div className="check-box">
-                <input id="checkbox-2" type="checkbox" />
+                <input id="checkbox-2" type="checkbox" name="futsal" />
                 <label for="checkbox-2">Futsal</label>
               </div>
               <div className="check-box">
-                <input id="checkbox-3" type="checkbox" />
+                <input id="checkbox-3" type="checkbox" name="võlei" />
                 <label for="checkbox-3">Vôlei</label>
               </div>
               <div className="check-box">
-                <input id="checkbox-4" type="checkbox" />
+                <input id="checkbox-4" type="checkbox" name="handelbol" />
                 <label for="checkbox-4">Handebol</label>
               </div>
               <div className="check-box">
-                <input id="checkbox-5" type="checkbox" />
+                <input id="checkbox-5" type="checkbox" name="atletismo" />
                 <label for="checkbox-5">Atletismo</label>
               </div>
               <div className="check-box">
-                <input id="checkbox-6" type="checkbox" />
+                <input id="checkbox-6" type="checkbox" name="xadrez" />
                 <label for="checkbox-6">Xadrez</label>
               </div>
               <div className="check-box">
-                <input id="checkbox-7" type="checkbox" />
+                <input id="checkbox-7" type="checkbox" name="truco" />
                 <label for="checkbox-7">Truco</label>
               </div>
               <div className="check-box">
-                <input id="checkbox-8" type="checkbox" />
+                <input id="checkbox-8" type="checkbox" name="E-sports" />
                 <label for="checkbox-8">E-sports</label>
               </div>
               <div className="check-box">
-                <input id="checkbox-9" type="checkbox" />
+                <input id="checkbox-9" type="checkbox" name="outros" />
                 <label for="checkbox-9">Outros</label>
               </div>
             </div>
           </div>
-          <button type="submit" onclick={handleSubmit}></button>
+          <input type="submit" onclick={handleSubmit}></input>
         </form>
       </div>
     </div>
